2e27f0a1951be39bd4294b95c5b5284a0bfc76ce
yessenia <yejiga2006@gmail.com>
2023-04-24 20:16:34 -0500

    se creó el timer

--- pomodoro/main.js
+++ pomodoro/main.js
@@ -1,19 +1,20 @@
-const task = [];
+const tasks = [];
 let time = 0;
 let timer = null;
 let timerBreak = null;
 let current = null;
 
 const bAdd = document.querySelector("#bAdd");
-const itTask = document.querySelector("itTask");
+const itTask = document.querySelector("#itTask");
 const form = document.querySelector("#form");
+const taskName = document.querySelector("#time #taskName");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (itTask.value !== "") {
     createTask(itTask.value);
     itTask.value = "";
-    renderTask();
+    renderTasks();
   }
 });
 
@@ -24,21 +25,66 @@ function createTask(value) {
     completed: false,
   };
 
-  task.unshift(newTask);
+  tasks.unshift(newTask);
 }
-function renderTask() {
-  const html = task.map((task) => {
+function renderTasks() {
+  const html = tasks.map((task) => {
     return `
         <div class="task">
             <div class="completed">${
               task.completed
-                ? `<span class=done> Done </span>`
-                : `<button class="start-button" data-id="${task.id}">Start</button>`
-            }</div>
+                ? "<span class='done'> Done </span>"
+                : `<button class="start-button" data-id="${task.id}">Start</button></div>`
+            }
             <div class="title">${task.title}</div>
         </div>
     `;
   });
 
-  const taskContainer = document.querySelector("#tasks");
+  const tasksContainer = document.querySelector("#tasks");
+  tasksContainer.innerHTML = html.join("");
+
+  const startButtons = document.querySelectorAll(".task .start-button");
+  startButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      if (!timer) {
+        const id = button.getAttribute("data-id");
+        startButtonHandler(id);
+        button.textContent = "In progress...";
+      }
+    });
+  });
+}
+
+function startButtonHandler(id) {
+  time = 5;
+  current = id;
+  const taskIndex = tasks.findIndex((task) => task.id === id);
+  const taskName = document.querySelector("#time #taskName");
+  taskName.textContent = tasks[taskIndex].title;
+
+  timer = setInterval(() => {
+    timerHandler(id);
+  }, 1000);
+}
+
+function timerHandler(id) {
+  time--;
+  renderTime();
+
+  if (timer === 0) {
+    clearInterval(timer);
+
+    renderTasks();
+  }
+}
+
+function renderTime() {
+  const timeDiv = document.querySelector("#time #value");
+  const minutes = parseInt(time / 60);
+  const seconds = parseInt(time % 60);
+
+  timeDiv.textContent = `${minutes < 10 ? "0" : ""}${minutes}:${
+    seconds < 10 ? "0" : ""
+  }${seconds}`;
 }

de4c38d28027cbcc0482c4b33c090a0b2c68bb27
yessenia <yejiga2006@gmail.com>
2023-04-26 21:42:49 -0500

    incio proyecto conteoRegresivo

---
+++ conteoRegresivo/main.js
@@ -0,0 +1,60 @@
+let events = [];
+let arr = []; //aquí cargaremos info
+
+const eventName = document.querySelector("#eventName");
+const eventDate = document.querySelector("#evenDate");
+const eventbAdd = document.querySelector("#bAdd");
+const eventsContainer = document.querySelector("#eventsContainer");
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  addEvent();
+});
+
+function addEvent() {
+  if (eventName.value === "" || eventDate.value === "") {
+    return;
+  }
+
+  if (dateDiff(eventDate.value) < 0) {
+    return;
+  }
+
+  const newEvent = {
+    id: (Math.random() * 100).toString(36).slice(3),
+    name: eventName.value,
+    date: eventDate.value,
+  };
+  events.unshift(newEvent);
+  eventName.value = "";
+  renderEvents();
+}
+
+function dateDiff(d) {
+  const targetDate = new Date(d);
+  const today = new Date();
+  const difference = targetDate.getTime() - today.getTime();
+  const days = Match.ceil(difference / (1000 * 3600 * 24));
+
+  return days;
+}
+
+function renderEvents() {
+  const eventsHTML = events.mao((event) => {
+    return `
+      <div class="event">
+        <div class="days">
+          <span class="days-number">${dateDiff(event.date)}</span>
+           <span class="days-text">Días</span>
+        </div>
+
+          <div class="event-name">${event.name} </div>
+          <div class="event-name">${event.date} </div>
+          <div class="actions">
+            <button></button>
+          </div>  
+        
+      </div>
+    `;
+  });
+}

655ce7410263ac3c14a007650db488774debd0ba
yessenia <yejiga2006@gmail.com>
2023-05-09 16:32:32 -0500

    componente de tags terminado

--- componenteTags/main.js
+++ componenteTags/main.js
@@ -22,3 +22,54 @@ inputTagContainer.addEventListener("click", (e) => {
     inputTag.focus();
   }
 });
+
+inputTag.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && inputTag.textContent !== "") {
+    e.preventDefault();
+    if (!existTag(inputTag.textContent)) {
+      tags.push(inputTag.textContent);
+      inputTag.textContent = "";
+      renderTags();
+    }
+  } else if (
+    e.key === "Backspace" &&
+    inputTag.textContent === "" &&
+    tags.length > 0
+  ) {
+    tags.pop();
+    renderTags();
+  }
+});
+
+function renderTags() {
+  tagsContainer.innerHTML = "";
+  const html = tags.map((tag) => {
+    const tagElement = document.createElement("div");
+    const tagButton = document.createElement("button");
+
+    tagElement.classList.add("tag-item");
+    tagButton.textContent = "X";
+    tagButton.addEventListener("click", (e) => {
+      removeTag(tag);
+    });
+    tagElement.appendChild(document.createTextNode(tag));
+    tagElement.appendChild(tagButton);
+
+    return tagElement;
+  });
+
+  html.forEach((element) => {
+    tagsContainer.appendChild(element);
+  });
+  tagsContainer.appendChild(inputTag);
+  inputTag.focus();
+}
+
+function existTag(value) {
+  return tags.includes(value);
+}
+
+function removeTag(value) {
+  tags = tags.filter((tag) => tag !== value);
+  renderTags();
+}

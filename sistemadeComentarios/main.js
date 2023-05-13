c2666eb928e41c9904a3d9fd86b395bdae7a511a
yessenia <yejiga2006@gmail.com>
2023-05-12 23:33:34 -0500

    avance sistema comentarios

--- sistemadeComentarios/main.js
+++ sistemadeComentarios/main.js
@@ -2,7 +2,7 @@ const comments = [];
 
 const inputContainer = document.createElement("div");
 const input = document.createElement("input");
-commentsContainer = document.querySelector("#comments-container");
+const commentsContainer = document.querySelector("#comments-container"); // Declara commentsContainer como variable local
 input.classList.add("input");
 
 input.addEventListener("keydown", (e) => {
@@ -25,10 +25,44 @@ function handleEnter(e, current) {
     e.target.value = "";
     commentsContainer.innerHTML = "";
     commentsContainer.appendChild(inputContainer);
-    console.log(comments);
 
     renderComments(comments, commentsContainer);
   }
 }
-console.log(comments);
-function renderComments(arr, parent) {}
+
+function renderComments(arr, parent) {
+  arr.forEach((element) => {
+    const commentContainer = document.createElement("div");
+    commentContainer.classList.add("comment-container");
+
+    const responsesContainer = document.createElement("div");
+    responsesContainer.classList.add("responses-container");
+
+    const replyButton = document.createElement("button");
+    const likeButton = document.createElement("button");
+    const textContainer = document.createElement("div");
+    textContainer.textContent = element.text;
+
+    const actionsContainer = document.createElement("div");
+
+    replyButton.textContent = "Reply";
+    likeButton.textContent = `${
+      element.likes > 0 ? `${element.likes} likes` : "Like"
+    }`; // Usa element.likes en lugar de element.liked
+
+    replyButton.addEventListener("click", (e) => {});
+    likeButton.addEventListener("click", (e) => {});
+
+    commentContainer.appendChild(textContainer);
+    commentContainer.appendChild(actionsContainer);
+    actionsContainer.appendChild(replyButton);
+    actionsContainer.appendChild(likeButton);
+
+    commentContainer.appendChild(responsesContainer);
+
+    if (element.responses.length > 0) {
+      renderComments(element.responses, responsesContainer);
+    }
+    parent.appendChild(commentContainer);
+  });
+}

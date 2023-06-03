8dcca178a66c8421790f6cdec4309e0b2ca7b09a
yessenia <yejiga2006@gmail.com>
2023-06-03 00:26:13 -0500

    new funcionalities added

---
+++ carritoCompras/main.js
@@ -0,0 +1,63 @@
+const db = {
+  methods: {
+    find: (id) => {
+      return db.items.find((item) => item.id === id);
+    },
+    remove: (id) => {
+      items.forEach((item) => {
+        const product = db.methods.find(item.id);
+        product.qty = product.qty - item - qty;
+      });
+      console.log(db);
+    },
+  },
+  items: [
+    {
+      id: 0,
+      title: "Funko Pop",
+      price: 250,
+      qty: 5,
+    },
+    {
+      id: 1,
+      title: "pistola burbujas",
+      price: 150,
+      qty: 5,
+    },
+    {
+      id: 2,
+      title: "mouse",
+      price: 300,
+      qty: 5,
+    },
+  ],
+};
+
+const shoppingCart = {
+  items: [],
+  methods: {
+    add: (id, qty) => {
+      const cartItem = shoppingCart.methods.get(id);
+      if (cartItem) {
+        if (shoppingCart.methods.hasInventory(id, qty + cartItem.qty)) {
+          cartItem.qty += qty;
+        } else {
+          alert("no hay suficientes unidades de este producto");
+        }
+      } else {
+        shoppingCart.items.push({ id, qty });
+      }
+    },
+    remove: (id, qty) => {},
+    count: () => {},
+    get: (id) => {
+      const index = shoppingCart.items.findIndex((item) => item.id === id);
+      return index >= 0 ? shoppingCart.items[index] : null;
+    },
+    getTotal: () => {},
+    hasInventory: (id, qty) => {
+      return db.items.find((item) => item.id === id).qty - qty >= 0;
+    },
+    purchase: () => {},
+  },
+};
